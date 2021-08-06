0d84ed42146330c3ba52fd814a9551bba9ffc580
RooHaaan <87962839+RooHaaan@users.noreply.github.com>
2021-08-06 10:20:37 +0600

    Add files via upload

---
+++ js/App.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.navigationBar a:eq(0)').css({
+       "borderBottom":"1px solid white"
+    })
+});
+
+
+//sitesection
+$(document).ready(function(){
+    $('#SiteBtn').click(function(){
+        $(this).css({
+            "borderrBottom":"white"
+        })
+    })
+});
